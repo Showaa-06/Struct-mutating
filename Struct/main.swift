f4b27c39889da5b7bceb0711ed754b9e4a2f47f9
Showaa-06 <115485438+Showaa-06@users.noreply.github.com>
2022-10-29 19:07:52 +0900

    complete

--- Struct/main.swift
+++ Struct/main.swift
@@ -1,11 +1,33 @@
-//
-//  main.swift
-//  Struct
-//
-//  Created by 鈴木彰悟 on 2022/10/29.
-//
 
-import Foundation
+struct Town {
+    //Property  //プロパティー  //self
+        //Property  //プロパティー
+        let name: String
+        var shop: [String]
+        var farmproducts: [String: Int]
+        
+        init(name: String, shop: [String], farmproducts: [String: Int]) {
+            
+            self.name = name
+            self.shop = shop
+            self.farmproducts = farmproducts
+        }
+    //self内を変えようとしているから mutatingが必要
+    //selfは省略化
+    mutating func addFarmProducts() {
+        self.farmproducts["potato"] = 200
+        
+            
+        }
+    
+        
+    }
+    
+    
+    var myTown = Town(name: "shoTown", shop:["Sportshop","supermarket"],farmproducts: ["carrots": 200])
+myTown.addFarmProducts()
+print(myTown.farmproducts)
+print(myTown.shop)
 
-print("Hello, World!")
-
+    
+    
